592547ae07526bbef7c60d50df2b756af10ef954
Leong Wee Hong <78012191+Zejakob123@users.noreply.github.com>
2025-04-02 18:00:00 +0800

    Firebase installed
    - Could run with firebase installed, the reason unable to run previously is the latest firebase installed must be run on minSdk = 23 instead of 21 (flutter.minSdk), and if do dependency_overrides on firebase_core_platform_interface, the latest firebase should be worked properly with

--- application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import device_info_plus
 import devicelocale
 import file_selector_macos
 import firebase_analytics
+import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_app_badger
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
